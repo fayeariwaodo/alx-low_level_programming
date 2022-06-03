dc8dc0eea79e15fa426f87913b54a516f654ffaa
Faith <fayeabache@gmail.com>
2022-06-02 18:05:18 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(void)
+/**
+ * main - Entry point
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ * Return: Always 0 (Success)
+ */
+
+int main(voidi)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 62);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
