b6114fde0b831a4eb7ea3478d26ee0217c1348ec
Faith <fayeabache@gmail.com>
2022-06-09 02:32:46 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,14 +3,15 @@
 
 /**
  * main - test function for positive or negative
- * Return: Always 0
+ * Return: 0
  */
 
-int main(void)
-{
+	int main(void)
+	{
+
 	int i;
 	i = 0;
 	positive_or_negative(i);
 	
 	return (0);
-}
+	}
