3c10a151c489e7800e1d4efcf295abb75bb9f9c9
Faith <fayeabache@gmail.com>
2022-06-09 05:10:57 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 	int i;
-
-	printf("Infinite loop incoming :(\n");
-
-	i = 0;
 	/**
+	printf("Infinite loop incoming :(\n");
+	*/
+	i = 0;
+	
 	while (i < 10)
 	{
 		putchar(i);
 	}
-	*/
+	
 printf("Infinite loop avoided! \\0/\n");
 return (0);
 }
