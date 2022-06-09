d05d1400536c8eda9592379949da9b9d6205d0cc
Faith <fayeabache@gmail.com>
2022-06-09 03:35:35 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,17 @@
 
 
 /**
- * main - test function for positive or negative
+ * main - tests functionfuction that prints if integer is positive or negative
  * Return: 0
  */
 
-	int main(void)
-	{
+int main(void)
+{
 
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
 	
 	return (0);
-	}
+}
