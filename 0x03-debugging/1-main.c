789680c6033cfc9b63b792859db5ed0e67ecc539
Faith <fayeabache@gmail.com>
2022-06-09 05:04:16 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,26 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
- * main - causes an 
+ * main - causes annfinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	while (i < 10)
+
+
+
+	{
+		putchar(i);
+	}
+	*/
+	printf("Infinite loop avoided! \\0/\n");
+	return (0);
+}
