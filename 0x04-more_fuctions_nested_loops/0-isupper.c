bb9db59c89e57ab9d5950b4e41a8079e7ec1835b
Faith <fayeabache@gmail.com>
2022-06-09 16:06:46 -0700

    checks charater in uppercase

--- 0x04-more_fuctions_nested_loops/0-isupper.c
+++ 0x04-more_fuctions_nested_loops/0-isupper.c
@@ -3,15 +3,16 @@
 /**
  * _isupper - checks is a character is uppercase
  * @c: carrier integer variable
- *  * Return: 1 if true, 0 if false
+ * Return: 1 if c is uppercase
+ * Returns 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
 	return (0);
 
-}
+i}
