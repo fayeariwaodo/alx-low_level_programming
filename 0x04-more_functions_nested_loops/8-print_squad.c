8547f57fc1200d3200de96b9655d4fae02e73f68
Faith <fayeabache@gmail.com>
2022-06-10 13:08:36 -0700

    prints a square, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/8-print_squad.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ *  print_square - prints a square, followed by a new line.
+ *  @size: Size of square sides
+ *
+ *  Return: Nothing
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+	else
+		_putchar('\n');
+}
