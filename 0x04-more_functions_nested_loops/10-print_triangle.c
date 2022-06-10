185a7fbab781ef247cb58e05d1035d64dd3f1582
Faith <fayeabache@gmail.com>
2022-06-10 14:19:30 -0700

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+
+
+/**
+ * print_triangle - prints a triangle
+ * @size: Size of triangle
+ *
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int c, s;
+
+	if (size > 0)
+	{
+		c = 0;
+		while (c < size)
+		{
+			s = size - 1;
+			while (s > c)
+			{
+				_putchar(' ');
+				s--;
+			}
+			s = 0;
+			while (s < c + 1)
+			{
+				_putchar('#');
+				s++;
+			}
+			_putchar('\n');
+			c++;
+		}
+	}
+	else
+		_putchar('\n');
+}
