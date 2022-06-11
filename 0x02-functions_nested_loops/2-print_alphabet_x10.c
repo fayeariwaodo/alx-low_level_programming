bbd3ba0e6c18ac19fbf5f34b602e3a4a80a8f03c
Faith <fayeabache@gmail.com>
2022-06-11 07:52:50 -0700

    prints 10 times alphabet in lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,15 +13,15 @@ void print_alphabet_x10(void)
 
 	while (b < 10)
 	{
-		char a = 'a';
+	char a = 'a';
 
-		while (a <= 'z')
+	while (a <= 'z')
 
-		{
-			_putchar(a);
-			a++;
-		}
-		_putchar('\n');
-		b++;
+	{
+	_putchar(a);
+	a++;
+	}
+	_putchar('\n');
+	b++;
 	}
 }
