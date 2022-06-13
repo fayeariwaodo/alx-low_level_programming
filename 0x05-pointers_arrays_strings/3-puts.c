9c05568d28f3eda84b45a54d204db9661a9b6ca9
Faith <fayeabache@gmail.com>
2022-06-13 09:37:08 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - print given strings
+ * @faye: string to print
+ *
+ * Return: 0
+ */
+
+void _puts(char *faye)
+{
+	int i;
+
+	while(*(faye + i) != '\0')
+	{
+		_putchar(*(faye + i));
+		i++;
+	}
+	_putchar('\n');
+}
