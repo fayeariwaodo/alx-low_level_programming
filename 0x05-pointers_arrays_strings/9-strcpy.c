2b2ce4f7e075c672286d474e33dd458fe2d2e3ff
Faith <fayeabache@gmail.com>
2022-06-13 14:13:49 -0700

    copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * _strcpy - copiies the string pointed to by src
+ * @dest: copy string
+ * @src: copy string pointer
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do{
+		i++;
+		dest[i] = src[i];
+	}while (src[i] != '\0');
+
+	return (dest);
+}
