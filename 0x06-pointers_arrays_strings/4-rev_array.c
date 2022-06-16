b6c0046dfe6489ebc7b868f3d615d59f8760fa49
Faith <fayeabache@gmail.com>
2022-06-16 11:08:55 -0700

    reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ *@a : points to int parameter
+ * @n : points to int parameter
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = n - 1;
+	int tmp;
+
+
+	while (i < j)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
+	}
+}
