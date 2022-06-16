fb23a01d60e5f3e7fc293b465056d72eb13cb4c0
Faith <fayeabache@gmail.com>
2022-06-16 10:42:54 -0700

    copy two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,14 @@
 
 
 /**
- * strncpy - copy two strings
+ * _strncpy - copy two strings
  * @dest: points to char parameter
  * @src: points to the source of the char parameter
  * @n: integer
  * Return: *dest
  */
 
-char *strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 
 	int i;
