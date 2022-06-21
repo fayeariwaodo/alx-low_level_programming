dd574f40576239ec06a9d3057e7a43c908ad658a
Faith <fayeabache@gmail.com>
2022-06-21 06:01:18 -0700

    Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
- * _strchr - Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ * _strchr - Returns a pointer to the first occurrence
  * @s: points to char
  * @c: where to find char parameter
- * Return: *S
+ * Return: *s
  */
 
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
+}
